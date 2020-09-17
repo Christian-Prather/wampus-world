975292cf7e4afc71b90298cb4b46ff409e8ccd10
Christian Prather <christian@luci.com>
2020-09-17 15:35:39 -0600

    Working World

---
+++ build/CMakeFiles/Wampus_World.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wampus_World.dir/check_true_false.cpp.o"
+  "Wampus_World"
+  "Wampus_World.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wampus_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
